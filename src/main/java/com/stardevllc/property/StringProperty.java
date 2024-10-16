a05136df13c80f04c50040f857c943743f24c6e2
Firestar311 <slaterandrew0398@gmail.com>
2024-10-16 10:13:18 -0500

    Added constructors to the String and UUID properties because I forgot

--- src/main/java/com/stardevllc/property/StringProperty.java
+++ src/main/java/com/stardevllc/property/StringProperty.java
@@ -3,4 +3,19 @@ package com.stardevllc.property;
 import com.stardevllc.observable.writable.WritableStringValue;
 
 public class StringProperty extends ObjectProperty<String> implements WritableStringValue {
+
+    public StringProperty(Object bean, String name, String value) {
+        super(bean, name, value);
+    }
+
+    public StringProperty(String name, String value) {
+        super(name, value);
+    }
+
+    public StringProperty(String value) {
+        super(value);
+    }
+
+    public StringProperty() {
+    }
 }
--- src/main/java/com/stardevllc/property/UUIDProperty.java
+++ src/main/java/com/stardevllc/property/UUIDProperty.java
@@ -5,4 +5,19 @@ import com.stardevllc.observable.writable.WritableUUIDValue;
 import java.util.UUID;
 
 public class UUIDProperty extends ObjectProperty<UUID> implements WritableUUIDValue {
+
+    public UUIDProperty(Object bean, String name, UUID value) {
+        super(bean, name, value);
+    }
+
+    public UUIDProperty(String name, UUID value) {
+        super(name, value);
+    }
+
+    public UUIDProperty(UUID value) {
+        super(value);
+    }
+
+    public UUIDProperty() {
+    }
 }
